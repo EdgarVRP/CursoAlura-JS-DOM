519a2ce7b32d2fecb30b7fb791b78abcc4a61444
EdgarVRP <edgarvrp22@gmail.com>
2022-11-04 12:53:23 -0600

    clase 03 completada

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Para clase 1
 
-
-Para clase 2 
+Para clase 2
 
 git checkout -b "clase-02"
 
@@ -10,3 +9,13 @@ git status
 git add -A
 
 git commit -m "clase02 completada"
+
+Para clase 3
+
+git checkout -b "clase03"
+
+git status
+
+git add -A
+
+git commit -m "clase 03 completada"
--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const btn =document.querySelector("[data-form-btn]");
-const input =document.querySelector("[data-form-input]");
+
 console.log(btn);
 
 const createTask = (evento) => {
     evento.preventDefault();
-    console.log(input.value);
+    const input =document.querySelector("[data-form-input]");
+    const value= input.value;
+    const list =document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value="";
+    //backticks
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  console.log(content);
+  task.innerHTML= content;
+  list.appendChild(task);
+  
+  console.log(task);
 };
 
 btn.addEventListener("click", createTask);
