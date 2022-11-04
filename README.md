4d43129ef6c80f3f8548352955f7e1f53933847f
EdgarVRP <edgarvrp22@gmail.com>
2022-11-04 12:31:07 -0600

    clase02 completada

---
+++ README.md
@@ -0,0 +1,12 @@
+Para clase 1
+
+
+Para clase 2 
+
+git checkout -b "clase-02"
+
+git status
+
+git add -A
+
+git commit -m "clase02 completada"
--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const btn =document.querySelector("[data-form-btn]");
 const input =document.querySelector("[data-form-input]");
 console.log(btn);
 
+const createTask = (evento) => {
+    evento.preventDefault();
+    console.log(input.value);
+};
+
+btn.addEventListener("click", createTask);
+/*
+btn.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    console.log(input.value);
+});
+*/
+/*
 btn.addEventListener("click", function(evento){
     evento.preventDefault();
     console.log(input.value);
 });
+*/
