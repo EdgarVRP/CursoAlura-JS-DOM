9d3a55786ec7ceb8bcbca91049d0cd17cfdb04cf
EdgarVRP <edgarvrp22@gmail.com>
2022-11-05 11:02:45 -0600

    clase 03 completada

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn =document.querySelector("[data-form-btn]");
+(()=>{const btn =document.querySelector("[data-form-btn]");
 
 console.log(btn);
 
@@ -11,19 +11,37 @@ const createTask = (evento) => {
     task.classList.add("card");
     input.value="";
     //backticks
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+    console.log(checkComplete());
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const tittleTask = document.createElement("span");
+    tittleTask.classList.add("task");
+    tittleTask.innerText=value;
+    taskContent.appendChild(tittleTask);
+    const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`;
   console.log(content);
-  task.innerHTML= content;
+  //task.innerHTML= content;
+  task.appendChild(taskContent);
   list.appendChild(task);
   
   console.log(task);
 };
 
 btn.addEventListener("click", createTask);
+
+const checkComplete = (evento) => {
+    const  i=document.createElement("i");
+    //ERROR por multiple STRING i.classList.add("far fa-check-square icon");
+    i.classList.add("far","fa-check-square","icon");
+    /*
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    */
+    i.addEventListener("click",completeTask);
+    return i;
+};
+
 /*
 btn.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -36,3 +54,13 @@ btn.addEventListener("click", function(evento){
     console.log(input.value);
 });
 */
+
+const completeTask=(event)=>{
+    console.log(event.target);
+    const element=event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
+//Se usa IIFE Inmediatly Invoked Function Expression
+})();
